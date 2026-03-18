db6085c9c34de8e779beba14544d5efb582d0a07
Harry Hutapea <harrybhutapea@gmail.com>
2026-03-18 22:55:41 +0700

    add app.dart and homepage.dart and refactor flutter standard template

---
+++ lib/app.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+import 'package:sonus/homepage.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(colorScheme: .fromSeed(seedColor: Colors.deepPurple)),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
